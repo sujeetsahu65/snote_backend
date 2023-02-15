b4fa75b9f81761fb88de77294fb226da6de63a42
sujeet <jeetsahu65@gmail.com>
2023-02-15 13:49:03 +0530

    15-02-2023

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 conn();
 
 const app = express()
-const port = 3000
+const port = 5000
 
 
 app.use(express.json());//if we want to use request-body(in the auth endpoint)
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { model } = require('mongoose');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 // router.get("/",(req,res)=>{
 router.post("/",
@@ -11,17 +12,42 @@ router.post("/",
   body('email', 'Idiot! enter a valid email').isEmail(),
   // password must be at least 5 chars long
   body('password').isLength({ min: 5 }),
-  (req,res)=>{
-      // Finds the validation errors in this request and wraps them in an object with handy functions
+  async (req,res)=>{
+
      const errors = validationResult(req);
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+
+// Error part 1 (is error is from the req body)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-const user = User(req.body);
+
+try{
+
+// Error part 2 (is error msg is from the db-model based error)
+let user = await User.findOne({email:req.body.email});
+
+if(user){
+    return res.status(400).json({ errors: "Email already found." });
+}
+
+user =  User(req.body);
 // user.save().then(user=>res.json(user)).catch(err=>res.json({'error':"dupli val"}));
 user.save().then(user=>res.json(user)).catch(err=>res.json({'error':err, 'msg':err.message}));
+
+// we can also create user manually
+// User.create({
+//     name:req.body.name,
+//     email:req.body.email
+// })
 // console.log(req.body);
 // res.send(req.body);
+}
+catch(err){
+res.status(500).send("Some error occured");
+}
+
+
 })
  
 module.exports = router;
